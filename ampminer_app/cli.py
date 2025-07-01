d2325b8ff4c8c404b240654ef8554a29b8c06248
mario-benitez-prian <mario.benitezprian@gmail.com>
2025-07-01 17:55:21 +0200

    Modifica cly.py para eliminar uno de los argumentos no utilizado

--- ampminer_app/cli.py
+++ ampminer_app/cli.py
@@ -1,3 +1,6 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # 0 = all logs, 1 = filter INFO, 2 = filter WARNING, 3 = filter all except ERROR
+
 import argparse
 from ampminer_app.predictor import run_prediction
 
@@ -6,13 +9,11 @@ def main():
     parser.add_argument("fasta_file", help="Archivo FASTA de entrada con el proteoma completo.")
     parser.add_argument("model_file", help="Ruta al modelo entrenado (formato .h5).")
     parser.add_argument("output_file", help="Archivo FASTA de salida con las secuencias positivas.")
-    parser.add_argument("--max_length", type=int, default=300, help="Longitud máxima para las secuencias (por defecto: 300).")
     
     args = parser.parse_args()
 
     run_prediction(
         fasta_file=args.fasta_file,
         model_file=args.model_file,
-        output_file=args.output_file,
-        max_length=args.max_length
+        output_file=args.output_file
     )
