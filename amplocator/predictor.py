5985a41df6d8dd0111233dcaeff8ddccc1b95c83
mario-benitez-prian <mario.benitezprian@gmail.com>
2025-07-25 12:17:35 +0200

    Efectivamente al sacarlo fuera de run_prediction tensorflow se sigue cargando en la ayuda

--- amplocator/predictor.py
+++ amplocator/predictor.py
@@ -1,9 +1,3 @@
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-import pandas as pd
-import numpy as np
-from amplocator.preprocess_data import preprocess_fasta_sequences
-from amplocator.fasta_parser import read_fasta, write_fasta, write_precursor_predictions_table, write_locator_predictions_table, write_full_predictions_table
     
 def predict_precursors(headers, sequences, max_length, model_path):
 
@@ -79,6 +73,14 @@ def predict_amp_regions(headers, sequences, max_length, model_path):
 
 
 def run_prediction(fasta_file, output_prefix, mode):
+
+    import tensorflow as tf
+    from tensorflow.keras.models import load_model
+    import pandas as pd
+    import numpy as np
+    from amplocator.preprocess_data import preprocess_fasta_sequences
+    from amplocator.fasta_parser import read_fasta, write_fasta, write_precursor_predictions_table, write_locator_predictions_table, write_full_predictions_table
+
 
     print("[INFO] Reading input FASTA...")
     headers, sequences = read_fasta(fasta_file)
