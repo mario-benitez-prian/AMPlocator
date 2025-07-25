1c1d50c3ce38f4c6582d10eb7a2757041ef5d49f
mario-benitez-prian <mario.benitezprian@gmail.com>
2025-07-25 11:26:27 +0200

    He modificado el archivo setup.py para poner versiones minimas necesarias para utilizar la app. Ahora hay que probar si funciona

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "tensorflow==2.19.0",
-        "biopython",
-        "pandas",
-        "numpy"
+        "tensorflow-cpu>=2.11.0",  # Estable y compatible con la mayoría de CPUs modernas
+        "biopython>=1.79",         # Compatible con Python 3.8+ y muy estable
+        "pandas>=1.3.0",           # Buen soporte de funciones, sin romper compatibilidad
+        "numpy>=1.20.0"            # Compatible con TensorFlow 2.11+
     ],
     entry_points={
         "console_scripts": [
