955e6ce8f62bb29d30fb3047881dc84cc36155cf
mario-benitez-prian <mario.benitezprian@gmail.com>
2025-07-25 12:57:48 +0200

    FInal

--- amplocator/__init__.py
+++ amplocator/__init__.py
@@ -20,7 +20,7 @@
 
 tf = import_tensorflow() '''
     
-'''import os
+import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"'''
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
